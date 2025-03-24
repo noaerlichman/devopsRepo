d10d1dacdf39ad83037e1435e237c0e366d5880d
noaerlichman <noa270800@gmail.com>
2025-03-24 17:36:08 +0200

    Changed Noa

--- NoaErlichman.java
+++ NoaErlichman.java
@@ -1,7 +1,7 @@
 public class NoaErlichman{
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++) {
-			System.out.println("NoaErlichman! ..."+i);
+			System.out.println("NoaErlichman ..."+i);
 		}
 	}
 }
